31be2196fdecceda3401f9e8865a9e2e797fde1f
Salo <info@salo.com.de>
2026-01-02 10:19:18 +0100

    Remove no more used configuration

--- Configuration.py
+++ Configuration.py
@@ -3,7 +3,6 @@ config = {
         "DATASET_DIR": "Data",
         "IMAGE_HEIGHT": 224,
         "IMAGE_WIDTH": 224,
-        "DIR": "Signs",
         "BATCH_SIZE": 10,
         "CLASS_NAME_FILE": "ClassName.txt",
     },
